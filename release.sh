db9540199609caf862029f7f544d1c8175385633
Nathan Cutler <ncutler@suse.com>
2015-07-21 10:42:12 +0200

    release.sh: tweak release script
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- release.sh
+++ release.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-perl Build.PL
-./Build clean
+CPAN_NAME='App-Dochazka-Common'
+OBS_NAME='perl-App-Dochazka-Common'
+OBS_DIR="~/home:smithfarm/$OBS_NAME/"
 perl Build.PL
 ./Build distmeta
-./Build distclean
 ./Build dist
-( cd ~/home:smithfarm/perl-App-Dochazka-Common/ ; oosc rm App-Dochazka-Common-*.tar.gz )
-cp App-Dochazka-Common-*.tar.gz ~/home:smithfarm/perl-App-Dochazka-Common
-( cd ~/home:smithfarm/perl-App-Dochazka-Common/ ; oosc service dr ; oosc add App-Dochazka-Common-*.tar.gz ; oosc ci )
+( cd $OBS_DIR ; osc rm $CPAN_NAME-*.tar.gz )
+cp $CPAN_NAME-*.tar.gz $OBS_DIR
+./Build distclean
+( cd $OBS_DIR ; osc service dr ; osc add $CPAN_NAME-*.tar.gz ; osc -A https://api.opensuse.org/ commit )
