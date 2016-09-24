2ca1fc6387b0c64eb3e24930949789cdfbbb8ea4
Nathan Cutler <ncutler@suse.com>
2016-09-24 12:44:19 +0200

    0.206
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- lib/App/Dochazka/Common.pm
+++ lib/App/Dochazka/Common.pm
@@ -55,11 +55,11 @@ App::Dochazka::Common - Dochazka Attendance and Time Tracking System shared modu
 
 =head1 VERSION
 
-Version 0.205
+Version 0.206
 
 =cut
 
-our $VERSION = '0.205';
+our $VERSION = '0.206';
 
 
 
